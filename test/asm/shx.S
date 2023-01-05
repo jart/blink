jart/blink

commit 06997579261772479564bce6ef5127db0077ce9b

Improve performance another 30%
We now support and advertise support for MULX, ADCX, and ADOX because
they greatly enhance the performance of Cosmopolitan Libc cryptography.
Here's the latest elliptic curve and rsa benchmarks (see git history).

ECP test vectors Curve25519 .................. PASS (33,760us)
ECP test vectors secp256r1 rfc 5114 .......... PASS (9,172us)
ECP test vectors secp384r1 rfc 5114 .......... PASS (18,330us)
RSA Generate Key - 1024 bit key .............. PASS (425,171us)
RSA Generate Key - 2048 bit key .............. PASS (2,829,688us)

blink/blink.c
@@ -20,7 +20,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "blink/web.h"
 #include <unistd.h>
 
 #include "blink/assert.h"
@@ -39,6 +38,7 @@
 #include "blink/stats.h"
 #include "blink/syscall.h"
 #include "blink/util.h"
+#include "blink/web.h"
 #include "blink/xlat.h"
 
 #define OPTS "hjms"
@@ -49,6 +49,7 @@
   -m        enable memory safety\n\
   -s        print statistics on exit\n"
 
+extern char **environ;
 static bool FLAG_nojit;
 static bool FLAG_nolinear;
 

blink/bmi2.c
@@ -16,76 +16,59 @@
 │ TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR             │
 │ PERFORMANCE OF THIS SOFTWARE.                                                │
 ╚─────────────────────────────────────────────────────────────────────────────*/
-#include "blink/assert.h"
 #include "blink/endian.h"
 #include "blink/machine.h"
+#include "blink/modrm.h"
+#include "blink/mop.h"
 
-void OpCpuid(P) {
-  u32 ax, bx, cx, dx, jit;
-  ax = bx = cx = dx = 0;
-  switch (Get32(m->ax)) {
-    case 0:
-    case 0x80000000:
-      unassert(m->system->brand);
-      ax = 7;
-      bx = Read32((u8 *)(m->system->brand + 0));
-      dx = Read32((u8 *)(m->system->brand + 4));
-      cx = Read32((u8 *)(m->system->brand + 8));
-      break;
-    case 1:
-      cx |= 1 << 0;   // sse3
-      cx |= 1 << 1;   // pclmulqdq
-      cx |= 1 << 9;   // ssse3
-      cx |= 1 << 23;  // popcnt
-      cx |= 1 << 30;  // rdrnd
-      cx |= 0 << 25;  // aes
-      cx |= 1 << 13;  // cmpxchg16b
-      dx |= 1 << 0;   // fpu
-      dx |= 1 << 4;   // tsc
-      dx |= 1 << 6;   // pae
-      dx |= 1 << 8;   // cmpxchg8b
-      dx |= 1 << 15;  // cmov
-      dx |= 1 << 19;  // clflush
-      dx |= 1 << 23;  // mmx
-      dx |= 1 << 24;  // fxsave
-      dx |= 1 << 25;  // sse
-      dx |= 1 << 26;  // sse2
-      break;
-    case 7:
-      switch (Get32(m->cx)) {
-        case 0:
-          bx |= 1 << 0;   // fsgsbase
-          bx |= 1 << 8;   // bmi2
-          bx |= 1 << 9;   // erms
-          bx |= 1 << 18;  // rdseed
-          bx |= 1 << 19;  // adx
-          cx |= 1 << 22;  // rdpid
-          break;
-        default:
-          break;
-      }
-      break;
-    case 0x80000001:
-      jit = !IsJitDisabled(&m->system->jit);
-      cx |= 1 << 0;     // lahf
-      cx |= jit << 31;  // jit
-      dx |= 1 << 0;     // fpu
-      dx |= 1 << 8;     // cmpxchg8b
-      dx |= 1 << 11;    // syscall
-      dx |= 1 << 15;    // cmov
-      dx |= 1 << 23;    // mmx
-      dx |= 1 << 24;    // fxsave
-      dx |= 1 << 27;    // rdtscp
-      dx |= 1 << 29;    // long
-      break;
-    case 0x80000007:
-      dx |= 1 << 8;  // invtsc
-      break;
-    default:
-      break;
+// todo BZHI zero starting at bit, i.e. x&((1<<i)-1)
+//      MULX flagless unsigned multiply
+//      PDEP parallel bits deposit
+//      PEXT parallel bits extract
+// todo RORX flagless rotate right logical
+// todo SARX flagless shift arithmetic right
+// todo SHRX flagless shift logical right
+// todo SHLX flagless shift logical left
+
+static u64 Pdep(u64 x, u64 mask) {
+  u64 r, b;
+  for (r = 0, b = 1; mask; mask >>= 1, b <<= 1) {
+    if (mask & 1) {
+      if (x & 1) r |= b;
+      x >>= 1;
+    }
   }
-  Put64(m->ax, ax);
-  Put64(m->bx, bx);
-  Put64(m->cx, cx);
-  Put64(m->dx, dx);
+  return r;
+}
+
+static u64 Pext(u64 x, u64 mask) {
+  u64 r, b;
+  for (r = 0, b = 1; mask; mask >>= 1, x >>= 1) {
+    if (mask & 1) {
+      if (x & 1) r |= b;
+      b <<= 1;
+    }
+  }
+  return r;
+}
+
+static void OpPbit(P, u64 op(u64, u64)) {
+  if (Rexw(rde)) {
+    Put64(RegRexrReg(m, rde), op(Get64(RegVreg(m, rde)),
+                                 Load64(GetModrmRegisterWordPointerRead8(A))));
+  } else {
+    Put64(RegRexrReg(m, rde),
+          (u32)op(Get32(RegVreg(m, rde)),
+                  Load32(GetModrmRegisterWordPointerRead4(A))));
+  }
+}
+
+void Op2f5(P) {
+  if (Rep(rde) == 2) {
+    OpPbit(A, Pdep);
+  } else if (Rep(rde) == 3) {
+    OpPbit(A, Pext);
+  } else {
+    OpUdImpl(m);
+  }
 }

blink/disspec.c
@@ -1074,7 +1074,24 @@ const char *DisSpecMap2(struct XedDecodedInst *x, char *p) {
     RCASE(0x41, "phminposuw %Vdq Wdq");
     RCASE(0x80, "invept %Gq Mdq");
     RCASE(0x81, "invvpid %Gq Mdq");
-    RCASE(0xF6, "mulx Edqp %Bdqp %Gdqp");
+    case 0xF5:
+      if (Rep(x->op.rde) == 2) {
+        return "pdep %Gdqp %Bdqp Edqp";
+      } else if (Rep(x->op.rde) == 3) {
+        return "pext %Gdqp %Bdqp Edqp";
+      } else {
+        return "wut";
+      }
+    case 0xF6:
+      if (Osz(x->op.rde)) {
+        return "adcx %Gdqp Edqp";
+      } else if (Rep(x->op.rde) == 3) {
+        return "adox %Gdqp Edqp";
+      } else if (Rep(x->op.rde) == 2) {
+        return "mulx %Gdqp %Bdqp Edqp";
+      } else {
+        return "wut";
+      }
     case 0xF0:
       if (Rep(x->op.rde) == 2) {
         return "crc32 %Gvqp Eb";

blink/machine.c
@@ -2089,7 +2089,8 @@ nexgen32e_f GetOp(long op) {
       XLAT(0x21e, OpSsePabsd);
       XLAT(0x22a, OpMovntdqaVdqMdq);
       XLAT(0x240, OpSsePmulld);
-      XLAT(0x2f6, OpMulx);
+      XLAT(0x2f5, Op2f5);
+      XLAT(0x2f6, Op2f6);
       XLAT(0x30f, OpSsePalignr);
       XLAT(0x344, OpSsePclmulqdq);
       default:

blink/machine.h
@@ -521,7 +521,8 @@ void OpXaddEbGb(P);
 void OpXaddEvqpGvqp(P);
 void OpXchgGbEb(P);
 void OpXchgGvqpEvqp(P);
-void OpMulx(P);
+void Op2f5(P);
+void Op2f6(P);
 
 void *AllocateBig(size_t);
 void FreeBig(void *, size_t);
@@ -603,5 +604,6 @@ void OpTest(P);
 void OpAlui(P);
 i64 FastAnd8(struct Machine *, u64, u64);
 i64 FastSub8(struct Machine *, u64, u64);
+void Mulx64(u64, struct Machine *, long, long);
 
 #endif /* BLINK_MACHINE_H_ */

blink/x86.c
@@ -414,24 +414,17 @@ static int xed_vex_opcode_scanner(struct XedDecodedInst *x, int map) {
   return 0;
 }
 
-static void xed_vex_set_args(struct XedDecodedInst *x, int ymm, unsigned trips,
-                             unsigned vexdest210) {
-  x->op.rde |= ymm << 30;
-  x->op.rde |= (u64)trips << 59;
-  x->op.rde |= (u64)vexdest210 << 60;
-}
-
 static int xed_vex_c4_scanner(struct XedDecodedInst *x, int *imm_width,
                               int *vexvalid) {
   unsigned length, b1, b2;
-  int map, rexr, rexx, rexb, rexw, ymm, trips, vexdest210;
+  int map, rexr, rexx, rexb, rexw, ymm, vrex, vexdest210;
   if (xed_is_bound_instruction(x)) return 0;
   length = x->length;
   length++;
   if (length + 2 < x->op.max_bytes) {
     // map:   5-bit
     // rex.b: 1-bit (expands r/m or srm register operand)
-    // rex.x: 1-bit (expands sib or vex register operand)
+    // rex.x: 1-bit (expands sib register operands)
     // rex.r: 1-bit (expands reg register operand)
     b1 = x->bytes[length];
     rexr = !(b1 & 128);
@@ -440,11 +433,11 @@ static int xed_vex_c4_scanner(struct XedDecodedInst *x, int *imm_width,
     // prefix:        2-bit → {none, osz, rep3, rep2}
     // vector_length: 1-bit → {xmm, ymm}
     // vexdest210:    3-bit (second reg operand, inverted)
-    // vexdest3:      1-bit (if true has three operands)
+    // vrex:          1-bit a.k.a. vexdest3
     // rex.w:         1-bit (for 64-bit registers)
     b2 = x->bytes[length + 1];
     rexw = !!(b2 & 128);
-    trips = !!(b2 & 64);
+    vrex = !(b2 & 64);
     vexdest210 = (~b2 >> 3) & 7;
     ymm = !!(b2 & 4);
     xed_set_vex_prefix(x, b2 & 3);
@@ -452,9 +445,10 @@ static int xed_vex_c4_scanner(struct XedDecodedInst *x, int *imm_width,
     if ((b1 & 3) == XED_ILD_MAP3) {
       *imm_width = xed_bytes2bits(1);
     }
-    x->op.rde |= (u64)rexx << 63 | rexx << 17 | rexb << 15 | rexb << 10 |
+    x->op.rde |= (u64)vrex << 63 | rexx << 17 | rexb << 15 | rexb << 10 |
                  rexw << 6 | rexr << 3;
-    xed_vex_set_args(x, ymm, trips, vexdest210);
+    x->op.rde |= ymm << 30;
+    x->op.rde |= (u64)vexdest210 << 60;
     *vexvalid = 1;
     length += 2;
     x->length = length;
@@ -467,7 +461,7 @@ static int xed_vex_c4_scanner(struct XedDecodedInst *x, int *imm_width,
 
 static int xed_vex_c5_scanner(struct XedDecodedInst *x, int *vexvalid) {
   unsigned length, b;
-  int rexr, ymm, trips, vexdest210;
+  int rexr, ymm, vrex, vexdest210;
   length = x->length;
   if (xed_is_bound_instruction(x)) return 0;
   length++;
@@ -475,16 +469,17 @@ static int xed_vex_c5_scanner(struct XedDecodedInst *x, int *vexvalid) {
     // prefix:        2-bit → {none, osz, rep3, rep2}
     // vector_length: 1-bit → {xmm, ymm}
     // vexdest210:    3-bit
-    // vexdest3:      1-bit
+    // vrex:          1-bit
     // rex.r:         1-bit
     b = x->bytes[length];
     rexr = !(b & 128);
-    trips = !!(b & 64);
+    vrex = !!(b & 64);
     vexdest210 = (~b >> 3) & 7;
     ymm = (b >> 2) & 1;
     xed_set_vex_prefix(x, b & 3);
-    x->op.rde |= (u64)rexr << 63 | rexr << 3;
-    xed_vex_set_args(x, ymm, trips, vexdest210);
+    x->op.rde |= (u64)vrex << 63 | rexr << 3;
+    x->op.rde |= ymm << 30;
+    x->op.rde |= (u64)vexdest210 << 60;
     *vexvalid = 1;
     length++;
     x->length = length;

test/blink/x86_test.c
@@ -1099,10 +1099,9 @@ TEST(ild, vex) {
   EXPECT_EQ(0, DecodeInstruction(&xedd, code, 15, XED_MODE_LONG));
   EXPECT_EQ(5, xedd.length);
   EXPECT_EQ(2 << 8 | 0xf6, Mopcode(xedd.op.rde));
-  EXPECT_TRUE(Trips(xedd.op.rde));      // three params
   EXPECT_EQ(3, ModrmMod(xedd.op.rde));  // register operand
   EXPECT_TRUE(Rexw(xedd.op.rde));       // 64-bit
   EXPECT_EQ(1, ModrmReg(xedd.op.rde));  // rcx (arg1)
-  EXPECT_EQ(2, Vexarg(xedd.op.rde));    // rdx (arg2)
+  EXPECT_EQ(2, Vreg(xedd.op.rde));      // rdx (arg2)
   EXPECT_EQ(3, ModrmRm(xedd.op.rde));   // rbx (arg3)
 }

blink/alu.h
@@ -124,4 +124,9 @@ i64 Rcl64(struct Machine *, u64, u64);
 
 u64 BsuDoubleShift(struct Machine *, int, u64, u64, u8, bool);
 
+i64 Adcx32(u64, u64, struct Machine *);
+i64 Adcx64(u64, u64, struct Machine *);
+i64 Adox32(u64, u64, struct Machine *);
+i64 Adox64(u64, u64, struct Machine *);
+
 #endif /* BLINK_ALU_H_ */

commit 79c3ac904001e09e60cdefef8d765dcdd22e6280

Implement the rest of BMI2
Blink now supports the BZHI, SHLX, SHRX, and SARX instructions. Adding
these opcodes gets Qemu working again under Blink, which was broken by
06997579261772479564bce6ef5127db0077ce9b.

blink/blink.c
@@ -153,6 +153,7 @@ static void HandleSigs(void) {
 #ifndef __SANITIZE_THREAD__
   sa.sa_sigaction = OnSigSegv;
   sa.sa_flags = SA_SIGINFO;
+  unassert(!sigaction(SIGILL, &sa, 0));
   unassert(!sigaction(SIGSEGV, &sa, 0));
 #endif
 }

blink/bmi2.c
@@ -18,18 +18,20 @@
 ╚─────────────────────────────────────────────────────────────────────────────*/
 #include "blink/endian.h"
 #include "blink/flags.h"
+#include "blink/log.h"
 #include "blink/machine.h"
 #include "blink/modrm.h"
 #include "blink/mop.h"
 
-// todo BZHI clearing starting at bit
-//      MULX flagless unsigned multiply
-//      PDEP parallel bits deposit
-//      PEXT parallel bits extract
-//      RORX flagless rotate right logical
-// todo SARX flagless shift arithmetic right
-// todo SHRX flagless shift logical right
-// todo SHLX flagless shift logical left
+// BMI2
+// BZHI clearing starting at bit
+// MULX flagless unsigned multiply
+// PDEP parallel bits deposit
+// PEXT parallel bits extract
+// RORX flagless rotate right logical
+// SARX flagless shift arithmetic right
+// SHRX flagless shift logical right
+// SHLX flagless shift logical left
 
 static u64 Pdep(u64 x, u64 mask) {
   u64 r, b;
@@ -64,19 +66,56 @@ static void OpPbit(P, u64 op(u64, u64)) {
   }
 }
 
+static void OpBzhi(P) {
+  int i;
+  u64 x;
+  bool sf;
+  bool cf = 0;
+  i = Get8(RegVreg(m, rde));
+  if (Rexw(rde)) {
+    x = Load64(GetModrmRegisterWordPointerRead8(A));
+    if (i < 64) {
+      x &= (1ull << i) - 1;
+    } else {
+      cf = 1;
+    }
+    sf = (i64)x < 0;
+  } else {
+    x = Load32(GetModrmRegisterWordPointerRead4(A));
+    if (i < 32) {
+      x &= (1u << i) - 1;
+    } else {
+      cf = 1;
+    }
+    sf = (i32)x < 0;
+  }
+  Put64(RegRexrReg(m, rde), x);
+  m->flags = SetFlag(m->flags, FLAGS_ZF, !x);
+  m->flags = SetFlag(m->flags, FLAGS_CF, cf);
+  m->flags = SetFlag(m->flags, FLAGS_SF, sf);
+  m->flags = SetFlag(m->flags, FLAGS_OF, false);
+}
+
 void Op2f5(P) {
   if (Rep(rde) == 2) {
     OpPbit(A, Pdep);
   } else if (Rep(rde) == 3) {
     OpPbit(A, Pext);
+  } else if (!Osz(rde)) {
+    OpBzhi(A);
+#ifndef TINY
   } else {
     OpUdImpl(m);
+#endif
   }
 }
 
 void OpRorx(P) {
   int i;
   u64 z;
+#ifndef TINY
+  if (Ymm(rde)) OpUdImpl(m);
+#endif
   if (Rexw(rde)) {
     u64 x = Load64(GetModrmRegisterWordPointerRead8(A));
     i = uimm0 & 63;
@@ -89,4 +128,75 @@ void OpRorx(P) {
     z = x;
   }
   Put64(RegRexrReg(m, rde), z);
+}
+
+static void OpShlx(P) {
+  int i;
+  u64 z;
+  i = Get8(RegVreg(m, rde));
+  if (Rexw(rde)) {
+    u64 x = Load64(GetModrmRegisterWordPointerRead8(A));
+    i &= 63;
+    if (i) x <<= i;
+    z = x;
+  } else {
+    u32 x = Load32(GetModrmRegisterWordPointerRead4(A));
+    i &= 31;
+    if (i) x <<= i;
+    z = x;
+  }
+  Put64(RegRexrReg(m, rde), z);
+}
+
+static void OpShrx(P) {
+  int i;
+  u64 z;
+  i = Get8(RegVreg(m, rde));
+  if (Rexw(rde)) {
+    u64 x = Load64(GetModrmRegisterWordPointerRead8(A));
+    i &= 63;
+    if (i) x >>= i;
+    z = x;
+  } else {
+    u32 x = Load32(GetModrmRegisterWordPointerRead4(A));
+    i &= 31;
+    if (i) x >>= i;
+    z = x;
+  }
+  Put64(RegRexrReg(m, rde), z);
+}
+
+static void OpSarx(P) {
+  int i;
+  u64 z;
+  i = Get8(RegVreg(m, rde));
+  if (Rexw(rde)) {
+    i64 x = Load64(GetModrmRegisterWordPointerRead8(A));
+    i &= 63;
+    if (i) x >>= i;
+    z = x;
+  } else {
+    i32 x = Load32(GetModrmRegisterWordPointerRead4(A));
+    i &= 31;
+    if (i) x >>= i;
+    z = (u32)x;
+  }
+  Put64(RegRexrReg(m, rde), z);
+}
+
+void OpShx(P) {
+#ifndef TINY
+  if (Ymm(rde)) OpUdImpl(m);
+#endif
+  if (Osz(rde)) {
+    OpShlx(A);
+  } else if (Rep(rde) == 2) {
+    OpShrx(A);
+  } else if (Rep(rde) == 3) {
+    OpSarx(A);
+#ifndef TINY
+  } else {
+    OpUdImpl(m);
+#endif
+  }
 }

blink/disspec.c
@@ -1074,26 +1074,6 @@ const char *DisSpecMap2(struct XedDecodedInst *x, char *p) {
     RCASE(0x41, "phminposuw %Vdq Wdq");
     RCASE(0x80, "invept %Gq Mdq");
     RCASE(0x81, "invvpid %Gq Mdq");
-    case 0xF5:
-      if (Rep(x->op.rde) == 2) {
-        return "pdep %Gdqp %Bdqp Edqp";
-      } else if (Rep(x->op.rde) == 3) {
-        return "pext %Gdqp %Bdqp Edqp";
-      } else if (!Osz(x->op.rde)) {
-        return "bzhi %Gdqp %Bdqp Edqp";
-      } else {
-        return "wut";
-      }
-    case 0xF6:
-      if (Osz(x->op.rde)) {
-        return "adcx %Gdqp Edqp";
-      } else if (Rep(x->op.rde) == 3) {
-        return "adox %Gdqp Edqp";
-      } else if (Rep(x->op.rde) == 2) {
-        return "mulx %Gdqp %Bdqp Edqp";
-      } else {
-        return "wut";
-      }
     case 0xF0:
       if (Rep(x->op.rde) == 2) {
         return "crc32 %Gvqp Eb";
@@ -1108,6 +1088,36 @@ const char *DisSpecMap2(struct XedDecodedInst *x, char *p) {
         return "movbe M %Gvqp";
       }
       break;
+    case 0xF5:
+      if (Rep(x->op.rde) == 2) {
+        return "pdep %Gdqp %Bdqp Edqp";
+      } else if (Rep(x->op.rde) == 3) {
+        return "pext %Gdqp %Bdqp Edqp";
+      } else if (!Osz(x->op.rde)) {
+        return "bzhi %Gdqp %Bdqp Edqp";
+      } else {
+        return "wut";
+      }
+    case 0xF6:
+      if (Osz(x->op.rde)) {
+        return "adcx %Gdqp Edqp";
+      } else if (Rep(x->op.rde) == 2) {
+        return "mulx %Gdqp %Bdqp Edqp";
+      } else if (Rep(x->op.rde) == 3) {
+        return "adox %Gdqp Edqp";
+      } else {
+        return "wut";
+      }
+    case 0xF7:
+      if (Osz(x->op.rde)) {
+        return "shlx %Gdqp %Bdqp Edqp";
+      } else if (Rep(x->op.rde) == 2) {
+        return "shrx %Gdqp %Bdqp Edqp";
+      } else if (Rep(x->op.rde) == 3) {
+        return "sarx %Gdqp %Bdqp Edqp";
+      } else {
+        return "wut";
+      }
     default:
       return UNKNOWN;
   }

blink/machine.c
@@ -2091,6 +2091,7 @@ nexgen32e_f GetOp(long op) {
       XLAT(0x240, OpSsePmulld);
       XLAT(0x2f5, Op2f5);
       XLAT(0x2f6, Op2f6);
+      XLAT(0x2f7, OpShx);
       XLAT(0x30f, OpSsePalignr);
       XLAT(0x344, OpSsePclmulqdq);
       XLAT(0x3f0, OpRorx);

blink/machine.h
@@ -523,6 +523,7 @@ void OpXchgGbEb(P);
 void OpXchgGvqpEvqp(P);
 void Op2f5(P);
 void Op2f6(P);
+void OpShx(P);
 void OpRorx(P);
 
 void *AllocateBig(size_t);

blink/x86.c
@@ -404,9 +404,6 @@ static int xed_vex_opcode_scanner(struct XedDecodedInst *x, int map) {
   if (Mode(x->op.rde) == XED_MODE_LONG && Rex(x->op.rde)) {
     return XED_ERROR_BAD_REX_PREFIX;
   }
-  if (Osz(x->op.rde) /* || Rep(x->op.rde) */) {
-    return XED_ERROR_BAD_LEGACY_PREFIX;
-  }
   if (Mode(x->op.rde) == XED_MODE_REAL) {
     return XED_ERROR_INVALID_MODE;
   }
@@ -431,10 +428,10 @@ static int xed_vex_c4_scanner(struct XedDecodedInst *x, int *imm_width,
     rexx = !(b1 & 64);
     rexb = (Mode(x->op.rde) == XED_MODE_LONG) & !(b1 & 32);
     // prefix:        2-bit → {none, osz, rep3, rep2}
-    // vector_length: 1-bit → {xmm, ymm}
+    // vector_length: 1-bit → {xmm, ymm} aka VEX.L
     // vexdest210:    3-bit (second reg operand, inverted)
     // vrex:          1-bit a.k.a. vexdest3
-    // rex.w:         1-bit (for 64-bit registers)
+    // rex.w:         1-bit (for 64-bit registers) aka VEX.W1
     b2 = x->bytes[length + 1];
     rexw = !!(b2 & 128);
     vrex = !(b2 & 64);

test/asm/bzhi.S
@@ -1,0 +1,23 @@
+#include "test/asm/mac.inc"
+.globl	_start
+_start:	mov	$10,%r15
+"test jit too":
+
+//	clear high bits
+//	make -j8 o//blink o//test/asm/bzhi.elf
+//	o//blink/blinkenlights o//test/asm/bzhi.elf
+
+	.test	"bzhi 1"
+	mov	$0xffffffff,%eax	# input value
+	mov	$0x31337133,%ebx	# random data
+	mov	$0xffffff08,%ecx	# clear bit 8 and above
+	bzhi	%ecx,%eax,%ebx
+	cmp	$0xffffffff,%eax	# unchanged
+	.e
+	cmp	$0x000000ff,%ebx	# output value
+	.e
+
+	dec	%r15
+	jnz	"test jit too"
+"test succeeded":
+	.exit

test/asm/shx.S
@@ -1,0 +1,49 @@
+#include "test/asm/mac.inc"
+.globl	_start
+_start:	mov	$10,%r15
+"test jit too":
+
+//	sarx/shlx/shrx -- shift w/o flags
+//	make -j8 o//blink o//test/asm/shx.elf
+//	o//blink/blinkenlights o//test/asm/shx.elf
+
+	.test	"shlx 1"
+	stc
+	mov	$0xf0000fff,%eax	# input value
+	mov	$0x31337133,%ebx	# random data
+	mov	$4,%ecx
+	shlx	%ecx,%eax,%ebx
+	.c
+	cmp	$0xf0000fff,%eax	# unchanged
+	.e
+	cmp	$0x0000fff0,%ebx	# output value
+	.e
+
+	.test	"shrx 1"
+	stc
+	mov	$0xf0000fff,%eax	# input value
+	mov	$0x31337133,%ebx	# random data
+	mov	$4,%ecx
+	shrx	%ecx,%eax,%ebx
+	.c
+	cmp	$0xf0000fff,%eax	# unchanged
+	.e
+	cmp	$0x0f0000ff,%ebx	# output value
+	.e
+
+	.test	"sarx 1"
+	stc
+	mov	$0xf0000fff,%eax	# input value
+	mov	$0x31337133,%ebx	# random data
+	mov	$4,%ecx
+	sarx	%ecx,%eax,%ebx
+	.c
+	cmp	$0xf0000fff,%eax	# unchanged
+	.e
+	cmp	$0xff0000ff,%ebx	# output value
+	.e
+
+	dec	%r15
+	jnz	"test jit too"
+"test succeeded":
+	.exit

test/blink/x86_test.c
@@ -1093,7 +1093,7 @@ TEST(ild, testWideNops) {
   EXPECT_EQ(9, ild("f\017\037\204\000\000\000\000\000", 9));
 }
 
-TEST(ild, vex) {
+TEST(ild, mulx) {
   // mulx %rbx,%rdx,%rcx
   char code[15] = "\xc4\xe2\xeb\xf6\xcb";
   EXPECT_EQ(0, DecodeInstruction(&xedd, code, 15, XED_MODE_LONG));
@@ -1104,4 +1104,18 @@ TEST(ild, vex) {
   EXPECT_EQ(1, ModrmReg(xedd.op.rde));  // rcx (arg1)
   EXPECT_EQ(2, Vreg(xedd.op.rde));      // rdx (arg2)
   EXPECT_EQ(3, ModrmRm(xedd.op.rde));   // rbx (arg3)
+}
+
+TEST(ild, shlx) {
+  // mulx %rbx,%rdx,%rcx
+  char code[15] = "\xc4\xe2\x71\xf7\xd8";
+  EXPECT_EQ(0, DecodeInstruction(&xedd, code, 15, XED_MODE_LONG));
+  EXPECT_EQ(5, xedd.length);
+  EXPECT_EQ(2 << 8 | 0xf7, Mopcode(xedd.op.rde));
+  EXPECT_EQ(3, ModrmMod(xedd.op.rde));  // register operand
+  EXPECT_TRUE(!Rexw(xedd.op.rde));      // 32-bit
+  EXPECT_EQ(3, ModrmReg(xedd.op.rde));  //
+  EXPECT_EQ(1, Vreg(xedd.op.rde));      //
+  EXPECT_EQ(0, ModrmRm(xedd.op.rde));   //
+  EXPECT_TRUE(Osz(xedd.op.rde));        //
 }