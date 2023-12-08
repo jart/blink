e75cbe84635e4c059444a502bbcf7143c343fec0
Jōshin <mrdomino@gmail.com>
2023-12-08 01:08:24 -0500

    Modelines (#158)
    Just the result of running the following:
    
    ```console
    rg 'vi:.* et' -l0 | \
      xargs -0 sed -i '' 's/vi:\(.*\) et\(.*\)  :vi/vi:\1 noet\2:vi/'
    rg 'vi:.*net' -l0 | \
      xargs -0 sed -i '' 's/vi:\(.*\)net\(.*\)  :vi/vi:\1et\2   :vi/'
    rg '\Svi:' -l0 | xargs -0 sed -i '' \
      's/\([^[:space:]]\)\(vi:.*\)  :vi\([^[:space:]]\)/\1 \2:vi \3/'
    rg '\Svi:' -l0 | \
      xargs -0 sed -i '' 's/[^[:space:]]\(vi:.*:vi\)[^[:space:]]/ \1 /'
    ```

--- third_party/ltp/ltp.mk
+++ third_party/ltp/ltp.mk
@@ -1,5 +1,5 @@
 #-*-mode:makefile-gmake;indent-tabs-mode:t;tab-width:8;coding:utf-8-*-┐
-#───vi: set et ft=make ts=8 tw=8 fenc=utf-8 :vi───────────────────────┘
+#── vi: set et ft=make ts=8 tw=8 fenc=utf-8 :vi ──────────────────────┘
 
 .PRECIOUS: third_party/ltp/%.gz
 third_party/ltp/%.gz: third_party/ltp/%.gz.sha256 o/tool/sha256sum
